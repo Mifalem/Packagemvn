7d7a072d65cc871c2f9f7018c5b8b3452754f2c9
My first test <mifalem@gmail.com>
2023-10-28 13:44:57 +0500

    calcrestadded

---
+++ src/main/java/ru/netology/services/HowToRestService.java
@@ -0,0 +1,22 @@
+package ru.netology.services;
+
+public class HowToRestService {
+    public int calculate(int income, int expenses, int threshold) {
+        int money = 0;
+        int count = 0;
+
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                money -= expenses;
+                money /= 3;
+                count++;
+            } else {
+                money += income;
+                money -= expenses;
+
+            }
+
+        }
+        return count;
+    }
+}
---
+++ src/test/java/ru/netology/services/HowToRestServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HowToRestServiceTest {
+    @Test
+    public void testcalc1() {
+        HowToRestService service = new HowToRestService();
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testcalc2() {
+        HowToRestService service = new HowToRestService();
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
